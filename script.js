4ab5c05ab48de85ee75cd0bbb6c96f533c19e522
Luiz Guilherme <luizgpelegatti@gmail.com>
2023-08-20 17:31:08 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function mudaR(){
+    const body = document.body;
+    const butbg = document.getElementById("bgbut");
+    body.classList.toggle("mode");
+    butbg.classList.toggle("darkmode");
+}
